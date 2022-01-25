27348e371f6d11c216072e58f7855043ec0897e5
c2h4balabala <97649921+c2h4balabala@users.noreply.github.com>
2022-01-25 23:39:29 +0800

    Add files via upload

---
+++ 伪代码 判断是否匹配.cpp
@@ -0,0 +1,23 @@
+//伪代码
+#include <stdio.h>
+int main()
+{
+	   int x=0;
+       long long yip,yipmax,yipmin,mip,mipmax,mipmin;
+	   long long yd,ydmax,ydmin,md,mdmin,mdmax;
+	   long long xie1,xie2;
+	   if(yipmin<=yip<=yipmax) 
+	   x++;
+	   if(mipmin<=mip<=mipmax)
+	   x++;
+	   if(ydmin<=yd<=ydmax)
+	   x++;
+	   if(mdmin<=md<=mdmax)
+	   x++;
+	   if(xie1==xie2)
+	   x++;
+	   if(x==5)
+	   //输出文件 
+	   else 
+	   //再次匹配 
+ } 
